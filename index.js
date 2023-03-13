d90b2aaca4855fe3ca52a86ed40447c2cbafe2bf
John <johnkaranjairungu@gmail.com>
2023-03-13 14:26:54 +0300

    done the assignment

---
+++ index.js
@@ -0,0 +1,25 @@
+a=int
+b=int
+var b=any
+function add() {
+    parseInt(a, b), 'adds two numbers and returns the result'
+    return a+b;
+  }
+  function subtract() {
+    subtract(a, b), 'subtracts b from a and returns the result'
+    return a-b;
+  }
+  function multiply() {
+    multiply(a, b), 'multiplies two numbers and returns the result'
+    return a*b
+  }
+  function divide() {
+    divide(a, b), 'divides a by b and returns the result'
+    return 12/3;
+  }
+  function increment(n) {
+    return (n+1)
+  }
+  function decrement(n) {
+    return (n-1)
+  }
